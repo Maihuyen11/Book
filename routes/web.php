1777106c79e127889c6dc7f8296ec39f6a2a243f
Maihuyen11 <030239230076@st.buh.edu.vn>
2026-03-31 10:50:26 +0700

    commit yc3

--- routes/web.php
+++ routes/web.php
@@ -69,3 +69,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+Route::get('/order','App\Http\Controllers\LayOutController@order')->name('order');
+Route::post('/cart/add','App\Http\Controllers\LayOutController@cartadd')->name('cartadd');
+Route::post('/cart/delete','App\Http\Controllers\LayOutController@cartdelete')->name('cartdelete');
+Route::post('/order/create','App\Http\Controllers\LayOutController@ordercreate') ->middleware('auth')->name('ordercreate');
