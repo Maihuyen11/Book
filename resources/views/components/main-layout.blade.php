ccc649e153f1c9853e054905346a6f5484fd4b5f
Thu Hung <030239230065@st.buh.edu.vn>
2026-03-31 12:23:00 +0700

    Gop code

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -1,36 +1,28 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>{{$title}}</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    
+<head>
+    <meta charset="UTF-8">
+    <title>{{ $title ?? 'Nhà Sách Phương Nam' }}</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-         <script src="https://code.jquery.com/jquery-3.7.1.js" ></script>
-        <style>
-            /* Định dạng màu nền và màu chữ của menu */
-            .navbar {
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+       .navbar {
                 background-color: #ff5850;
                 max-width:1000px;
                 font-weight:bold;
                 margin:0 auto;
 
             }
-            .nav-item a
-            {
-                color: #fff!important;
-            }
 
-<head>
-    <meta charset="UTF-8">
-    <title>{{ $title ?? 'Nhà Sách Phương Nam' }}</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <style>
-        .navbar { background-color: #ff5850; font-weight: bold; }
         .nav-item a { color: #fff !important; }
-        .navbar-nav { margin: 0 auto; }
+        .navbar-nav { margin: 0 auto;
+    width: 1000px; }
         
         /* Cập nhật CSS hiển thị danh sách sách theo tài liệu */
         .list-book { 
@@ -51,66 +43,6 @@
     </style>
 </head>
 <body>
-    <header style='text-align:center'>
-        <img src="{{ asset('image/banner_sach.jpg') }}" width="1000px">
-    </header>
-
-    <main style="width:1000px; margin:2px auto;">
-        <div class='row'>
-            <div class='col-3 pr-0'>
-                <nav class="navbar navbar-light">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link menu-the-loai" href="{{ url('sach') }}" the_loai="">Trang chủ</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link menu-the-loai" href="{{ url('sach') }}" the_loai="1">Tiểu thuyết</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link menu-the-loai" href="{{ url('sach') }}" the_loai="2">Truyện ngắn tản văn</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link menu-the-loai" href="{{ url('sach') }}" the_loai="3">Tác phẩm kinh điển</a>
-                        </li>
-                    </ul>
-                </nav>
-                <img src="{{ asset('image/sidebar_1.jpg') }}" width="100%" class='mt-1'>
-                <img src="{{ asset('image/sidebar_2.jpg') }}" width="100%" class='mt-1'>
-            </div>
-    <head>
-        <title>{{ $title ?? 'Nhà Sách Phương Nam'}}</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-        <style>
-            /* Định dạng màu nền và màu chữ của menu */
-            .navbar {
-                background-color: #ff5850;
-                max-width:1000px;
-                font-weight:bold;
-                margin:0 auto;
-
-            }
-            .nav-item a
-            {
-                color: #fff!important;
-            }
-
-            .list-book
-            {
-                display:grid;
-                grid-template-columns:repeat(5,20%);
-            }
-            .book
-            {
-            
-                margin:10px;
-                text-align:center;
-            }
-        </style>
-    </head>
-    <body>
         <header style='text-align:center'>
             <img src="{{asset('hinh/banner_sach.jpg')}}" width="1000px">
             <nav class="navbar navbar-light navbar-expand-sm">
