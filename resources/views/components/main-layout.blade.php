0250d3e1992521f72456130259d93ff5e7939ec5
camhong131 <030239230060@st.buh.edu.vn>
2026-03-31 13:26:34 +0700

    bổ sung yc4

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -5,10 +5,9 @@
     <meta charset="UTF-8">
     <title>{{ $title ?? 'Nhà Sách Phương Nam' }}</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
@@ -50,16 +49,16 @@
                     <div class='col-9 p-0'>
                             <ul class="navbar-nav">
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="{{url('sach')}}">Trang chủ</a>
+                                    <a class="nav-link menu-the-loai" href="{{url('sach')}}" the_loai="">Trang chủ</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{url('sach/theloai/1')}}">Tiểu thuyết</a>
+                                    <a class="nav-link menu-the-loai" href="{{url('sach')}}" the_loai="1">Tiểu thuyết</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{url('sach/theloai/2')}}">Truyện ngắn - tản văn</a>
+                                    <a class="nav-link menu-the-loai" href="{{url('sach')}}" the_loai="2">Truyện ngắn - tản văn</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{url('sach/theloai/3')}}">Tác phẩm kinh điển</a>
+                                    <a class="nav-link menu-the-loai" href="{{url('sach')}}" the_loai="3">Tác phẩm kinh điển</a>
                                 </li>
                             </ul>
                     </div>
@@ -109,12 +108,5 @@ border-radius:50%; position:absolute;right:2px;top:-2px' id='cart-number-product
                 </div>
             </div>
         </main>
-        <!--<footer>
-            <div class='row' style='text-align:center'>
-                <div class='col-4'>TRỤ SỞ</div>
-                <div class='col-4'>THÔNG TIN CHUNG</div>
-                <div class='col-4'>BẢN ĐỒ</div>
-            </div>
-        </footer>-->
-    </body>
+        </body>
 </html>
--- resources/views/sach/chitiet.blade.php
+++ resources/views/sach/chitiet.blade.php
@@ -3,7 +3,7 @@
 
     <div class="row">
         <div class="col-4">
-            <img src="{{ asset('hinh/image/'.$sach->file_anh_bia) }}" width="100%" class="img-thumbnail">
+            <img src="{{ asset('image/'.$sach->file_anh_bia) }}" width="100%" class="img-thumbnail">
         </div>
         <div class="col-8">
             <h2 class="text-danger">{{ $sach->tieu_de }}</h2>
@@ -12,34 +12,40 @@
             <p class="text-justify">{{ $sach->mo_ta }}</p>
             <hr>
             <a href="{{ url('sach') }}" class="btn btn-secondary">Quay lại danh sách</a>
-            <div class='mt-1'>
-                Số lượng mua:
-                <input type='number' id='product-number' size='5' min="1" value="1">
-                <button class='btn btn-success btn-sm mb-1' id='add-to-cart'>Thêm vào giỏ hàng</button>
-            </div>
+            <button class="btn btn-primary" id="add-to-cart">Thêm vào giỏ hàng</button>
         </div>
     </div>
-    <script>
-        $(document).ready(function(){
-        $("#add-to-cart").click(function(){
-            id = "{{$sach->id}}";
-            num = $("#product-number").val()
-            $.ajax({
-            type:"POST",
-            dataType:"json",
-            url: "{{route('cartadd')}}",
-            data:{"_token": "{{ csrf_token() }}","id":id,"num":num},
-            beforeSend:function(){
+<script>
+$(document).ready(function(){
+    $("#add-to-cart").click(function(e){
+        // 1. Ngăn chặn hành vi mặc định (tránh reload trang nếu nút nằm trong form)
+        e.preventDefault(); 
+        
+        let id = "{{ $sach->id }}"; 
+        let num = $("#product-number").length > 0 ? $("#product-number").val() : 1; 
+
+        $.ajax({
+            type: "POST",
+            // 2. Tắt dataType: "json" hoặc đổi thành "html" để JQuery không bị lỗi parse
+            dataType: "html", 
+            url: "{{ route('cartadd') }}",
+            data: {
+                "_token": "{{ csrf_token() }}", 
+                "id": id, 
+                "num": num
             },
-            success:function(data){
-                $("#cart-number-product").html(data);
+            success: function(data) {
+                // 3. Cập nhật số lượng mới nhất lên icon giỏ hàng
+                $("#cart-number-product").html(data); 
+                alert("Đã thêm sách vào giỏ hàng thành công!");
             },
-            error: function (xhr,status,error){
-            },
-            complete: function(xhr,status){
+            error: function (xhr, status, error) {
+                console.log("Lỗi AJAX:", error);
+                console.log("Chi tiết:", xhr.responseText);
+                alert("Có lỗi xảy ra, vui lòng thử lại!");
             }
-            });
-                    });
-                });
-    </script>
+        });
+    });
+});
+</script>
 </x-main-layout>
