a5db00dd25adaae363b5c4f0d4984929406849d9
030239230027-byte <030239230027@st.buh.edu.vn>
2026-03-31 10:40:16 +0700

    yeu cau 5

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LayOutController;
-
+use App\Http\Controllers\TestController;
 Route::get('/baoyen', function () {
     return 'Bảo Yến'; 
 });
